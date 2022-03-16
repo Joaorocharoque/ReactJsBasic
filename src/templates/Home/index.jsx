d2129cdcbee3c44aa07425627ca550fbf3737f5c
João Roque <joaorocharoque@gmail.com>
2022-03-16 14:57:15 -0300

    first commit

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+
+import "./styles.css";
+
+import { Posts } from "../../components/Posts";
+
+import { loadPosts } from "../../utils/load-post";
+import { Button } from "../../components/Button";
+
+export class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 2,
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+
+        <div className="button-container">
+          <Button text="load more posts" onClick={this.loadMorePosts} disabled={true}/>
+        </div>
+      </section>
+    );
+  }
+}
