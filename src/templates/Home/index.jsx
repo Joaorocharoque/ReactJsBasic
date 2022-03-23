fce105e5fdbe3db36247d04bc0ab5a1749503cae
João Roque <joaorocharoque@gmail.com>
2022-03-23 16:56:22 -0300

    atualizações 4

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -63,7 +63,7 @@ export class Home extends Component {
 
     return (
       <section className="container">
-        <h1>TE AMO MUITO AMOR</h1> <br></br>
+        <h1>Pagina de estudos</h1> <br></br>
         <div class="search-container">
           {!!searchValue && (
             <h1>Search value: {searchValue}</h1>
